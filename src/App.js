caa097ab1804addef9bc605d460106159a865621
chihebchabou <chihebchaboutech@gmail.com>
2023-02-01 12:05:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import Counter from './components/Counter';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Workshop State</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {
+    isVisible: false,
+  };
+
+  toggleVisibility = () => this.setState({ isVisible: !this.state.isVisible });
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Workshop State</h1>
+
+        <button className="visibility-btn" onClick={this.toggleVisibility}>
+          {this.state.isVisible ? 'Hide' : 'Show'} Counter
+        </button>
+        {/*this.state.isVisible === true ? <Counter /> : ''*/}
+        {this.state.isVisible && <Counter />}
+      </div>
+    );
+  }
 }
 
 export default App;
